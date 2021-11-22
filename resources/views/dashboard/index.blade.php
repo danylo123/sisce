b44f31986e663c52c3bcb02937da10d07e3294bd
danylo123 <danyloalyson1@gmail.com>
2021-11-22 14:38:02 -0300

    ajustes icones dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-3 col-sm-6">
     <div class="card-box bg-green">
-        <div class="inner">            
+        <div class="inner">
             <h3>
                 {{ $demolaysAtivos->count() }}
             </h3>
@@ -24,7 +24,15 @@
             <p> Capitulos</p>
         </div>
         <div class="icon">
-            <i class="fa fa-users" aria-hidden="true"></i>
+            <i aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                    role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                    <path
+                        d="M21 9v2h-2V3h-2v2h-2V3h-2v2h-2V3H9v2H7V3H5v8H3V9H1v12h9v-3c0-1.1.9-2 2-2s2 .9 2 2v3h9V9h-2zm0 10h-5v-1c0-2.21-1.79-4-4-4s-4 1.79-4 4v1H3v-6h4V7h10v6h4v6z"
+                        fill="currentColor" />
+                    <path d="M9 9h2v3H9zm4 0h2v3h-2z" fill="currentColor" />
+                </svg>
+            </i>
         </div>
         {{-- <button type="button" data-toggle="modal" data-target="#detailModal" wire:click="viewUser()"
             class="card-box-footer">Detalhar
@@ -41,7 +49,14 @@
             <p> Priorados</p>
         </div>
         <div class="icon">
-            <i class="fa fa-users" aria-hidden="true"></i>
+            <i aria-hidden="true">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
+                    role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                    <path
+                        d="M17.457 3L21 3.003l.002 3.523l-5.467 5.466l2.828 2.829l1.415-1.414l1.414 1.414l-2.474 2.475l2.828 2.829l-1.414 1.414l-2.829-2.829l-2.475 2.475l-1.414-1.414l1.414-1.415l-2.829-2.828l-2.828 2.828l1.415 1.415l-1.414 1.414l-2.475-2.475l-2.829 2.829l-1.414-1.414l2.829-2.83l-2.475-2.474l1.414-1.414l1.414 1.413l2.827-2.828l-5.46-5.46L3 3l3.546.003l5.453 5.454L17.457 3zm-7.58 10.406L7.05 16.234l.708.707l2.827-2.828l-.707-.707zm9.124-8.405h-.717l-4.87 4.869l.706.707l4.881-4.879v-.697zm-14 0v.7l11.241 11.241l.707-.707L5.716 5.002l-.715-.001z"
+                        fill="currentColor" />
+                </svg>
+            </i>
         </div>
         {{-- <button type="button" data-toggle="modal" data-target="#detailModal" wire:click="viewUser()"
             class="card-box-footer">Detalhar
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,13 @@
     Página inicial
 @stop
 @section('conteudo')
-    <div class="col-md-8">
-        <div class="row">
-            @include('dashboard.index')
-        </div>
+    <div class="col-md-10">
         <div class="row">
             @include('dashboard.avisos')
         </div>
-
+        <div class="row">
+            @include('dashboard.index')
+        </div>
     </div>
 
 @stop
