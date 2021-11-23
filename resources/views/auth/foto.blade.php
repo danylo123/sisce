3f84fd11059ca73d611f425ba995861d7e4444c7
danylo123 <danyloalyson1@gmail.com>
2021-11-23 08:13:46 -0300

    quando o usuario nao tiver foto, mostrar foto default no login

--- resources/views/auth/foto.blade.php
+++ resources/views/auth/foto.blade.php
@@ -1,3 +1,11 @@
-        @foreach ($fotos as $f)
+@if ($fotos->count() < 1)
+    <img src="{{ url('storage/icone/user.png') }}" alt="Usuário não encontrado" class="logo">
+@else
+    @foreach ($fotos as $f)
+        @if (empty($f->imagem))
+            <img src="{{ url('storage/icone/user.png') }}" alt="Usuário sem foto" class="logo">
+        @else
             <img src="{{ url('storage/users/' . $f->imagem) }}" alt="{{ $f->name }}" class="logo">
-        @endforeach
+        @endif
+    @endforeach
+@endif
