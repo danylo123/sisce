cd320a9f3b111e10bec95cd530903f1ed577b0c4
danylo123 <danyloalyson1@gmail.com>
2021-11-26 13:46:05 -0300

    criação paginas de erros

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Response;
 
 class Handler extends ExceptionHandler
 {
@@ -44,7 +45,32 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if ($this->isHttpException($exception)) {
+            switch ($exception->getStatusCode()) {
+
+                    // not authorized
+                case '403':
+                    return Response::view('errors.403', array(), 403);
+                    break;
+
+                    // not found
+                case '404':
+                    return Response::view('errors.404', array(), 404);
+                    break;
+
+                    // internal error
+                case '500':
+                    return Response::view('errors.500', array(), 500);
+                    break;
+
+                default:
+                    return $this->renderHttpException($exception);
+                    break;
+            }
+        } else {
+
+            return parent::render($request, $exception);
+        }
     }
 
     /**
